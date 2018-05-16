4e0293a850864d5e9266e3a94a9f0cf0c7bda761
Herb <i@4leaf.me>
2018-05-16 21:39:51 +0800

    使用 gm 替换 sharp

--- retina/build.js
+++ retina/build.js
@@ -1,28 +1,38 @@
 const path = require('path')
 const fs = require('fs')
 const glob = require('glob')
-const sharp = require('sharp')
+const gm = require('gm')
 
 module.exports = function (callback, baseDensity=3, densities = [1, 2], dir='src/assets/', ext='.png') {
-  glob(dir + '*' + ext, {}, function (er, files) {
+  glob(dir + '*' + ext, {}, function (err, files) {
     const promises = files.reduce(function (promises, file) {
       if (file.indexOf('@') >= 0) {
         return promises
       }
       densities.forEach(function (density) {
         const promise = new Promise(function (resolve, reject) {
-          const img = sharp(file)
-          img.metadata().then(function (metadata) {
-            const width = parseInt(metadata.width / baseDensity * density, 10)
+          const img = gm(file)
+          img.size(function (err, size) {
+            if (err) {
+              reject(err)
+              return
+            }
+            const width = parseInt(size.width / baseDensity * density, 10)
             const fileName = path.basename(file, ext) + '@' + density + 'x' + ext
             const filePath = path.join(path.dirname(file), fileName)
             if (fs.existsSync(filePath)) {
               resolve()
               return 
             }
-            img.clone().resize(
-              width, null).toFile(filePath).then(resolve).catch(reject)
-          }).catch(reject)
+            img.resize(
+              width, null).write(filePath, function (err) {
+                if (err) {
+                  reject(err)
+                } else {
+                  resolve()
+                }
+              })
+          })
         })
         promises.push(promise)
       })
