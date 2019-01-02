89a441656b85fc5a69a8901bd573e7603418c64d
Herb <i@4leaf.me>
2019-01-02 10:34:15 +0800

    七牛清理方式更新

--- qiniu-clean/index.js
+++ qiniu-clean/index.js
@@ -1,5 +1,6 @@
 const qiniu = require('qiniu')
 const pathUtil = require('path')
+const fs = require('fs')
 
 const min = 3
 
@@ -9,7 +10,9 @@ class QiniuCleaner {
     SECRET_KEY,
     bucket,
     path,
-    days
+    days,
+    delay,
+    exclude
   }) {
     if (!ACCESS_KEY || !SECRET_KEY) {
       throw new Error('ACCESS_KEY and SECRET_KEY must be provided');
@@ -28,23 +31,36 @@ class QiniuCleaner {
     this.bucket = bucket
     this.path = path
     this.days = parseInt(days, 10)
+    this.delay = delay
+    this.exclude = exclude
   }
 
   apply (compiler) {
     compiler.plugin('after-emit', (compilation, callback) => {
       const path = this.path.replace('[hash]', compilation.hash)
       const assets = compilation.assets
-      const fileNames = Object.keys(assets).filter((fileName) => {
+      let includes = Object.keys(assets).filter((fileName) => {
         return assets[fileName].emitted
       }).reduce((current, fileName) => {
         current[pathUtil.join(path, fileName)] = true
         return current
       }, {})
-      this.cleanIfNot(fileNames, path, undefined, callback)
+      if (this.delay) {
+        const file = this.delay
+        fs.readFile(file, 'utf8', function (err, data) {
+          if (data) {
+            const existIncludes = JSON.parse(data)
+            includes = Object.assign(existIncludes, includes)
+          }
+          fs.writeFile(file, JSON.stringify(includes), 'utf8', callback)
+        })
+      } else {
+        this.cleanIfNot(includes, this.exclude, path, undefined, callback)
+      }
     })
   }
 
-  cleanIfNot (fileNames, path, marker, callback) {
+  cleanIfNot (includes, exclude, path, marker, callback) {
     this.bucketManager.listPrefix(this.bucket, {
       prefix: path,
       marker: marker,
@@ -57,7 +73,10 @@ class QiniuCleaner {
       if (respInfo.statusCode === 200) {
         const items = respBody.items
         const operations = items.reduce((current, item) => {
-          if (fileNames[item.key] !== true) {
+          if (exclude && exclude.test(item.key)) {
+            return current
+          }
+          if (includes[item.key] !== true) {
             if (this.days > 0) {
               current.push(qiniu.rs.deleteAfterDaysOp(
                 this.bucket, item.key, this.days))
@@ -78,7 +97,7 @@ class QiniuCleaner {
             }
             if (parseInt(respInfo.statusCode / 100) === 2) {
               if (nextMarker) {
-                this.cleanIfNot(name, days, nextMarker, callback)
+                this.cleanIfNot(includes, exclude, path, nextMarker, callback)
               } else {
                 callback()
               }
@@ -88,13 +107,13 @@ class QiniuCleaner {
           })
         } else {
           if (nextMarker) {
-            this.cleanIfNot(fileNames, path, nextMarker, callback)
+            this.cleanIfNot(includes, exclude, path, nextMarker, callback)
           } else {
             callback()
           }
         }
       } else {
-        callback(new Error(respBody))
+        callback(new Error(JSON.stringify(respBody)))
       }
     })
   }
