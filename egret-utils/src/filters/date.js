477b9fa486e0c64d44c1b269f919b5d31cbd923f
Herb <i@4leaf.me>
2019-01-24 17:29:47 +0800

    open date format option

--- egret-utils/src/filters/date.js
+++ egret-utils/src/filters/date.js
@@ -1,12 +1,12 @@
 import format from 'date-fns/format'
 import parse from 'date-fns/parse'
 
-export default function (value, formatStr) {
+export default function (value, formatStr, option) {
   if (value) {
     if (typeof value.getMonth !== 'function') {
       value = parse(value)
     }
-    return format(value, formatStr)
+    return format(value, formatStr, option)
   } else {
     return null
   }
